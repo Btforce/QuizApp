f416734612273c44db3fe024a92ef5a1c0484690
Btforce <tashirobradley@gmail.com>
2019-10-11 14:09:04 -0700

    I tried my very best

---
+++ app/src/main/java/Question.java
@@ -0,0 +1,13 @@
+public class Question {
+
+    private String question;
+
+    public Question(){
+        this.question = question;
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+}
--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,10 +3,17 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.gson.Gson;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         InputStream jsonFileInputStream = getResources().openRawResource(R.raw.questions);
-        String jsonString =
+        String jsonString = readTextFile(jsonFileInputStream);
+
+        Log.d(TAG, "onCreate: " + jsonString);
+
+        Gson gson = new Gson();
+
     }
+
+    //reading the text file from
+    //https://stackoverflow.com/questions/15912825/how-to-read-file-from-res-raw-by-name
+    public String readTextFile(InputStream inputStream) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        byte buf[] = new byte[1024];
+        int len;
+        try {
+            while ((len = inputStream.read(buf)) != -1) {
+                outputStream.write(buf, 0, len);
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (IOException e) {
+
+        }
+        return outputStream.toString();
+    }
+
+
+
 }
